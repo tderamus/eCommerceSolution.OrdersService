eaf6bff3d0d60f7cc1e6fc6dc8bda770052af93c
tderamus <deramust@gmail.com>
2025-09-03 23:18:39 -0500

    added dependencies and entities to establish mongodb connections

--- DataAccessLayer/DependencyInjection.cs
+++ DataAccessLayer/DependencyInjection.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
 
 namespace eCommerce.OrderMicroservice.DataAccessLayer;
 
@@ -8,6 +9,20 @@ public static class DependencyInjection
     public static IServiceCollection AddDataAccessLayer(this IServiceCollection services, IConfiguration configuration)
     {
         // Add your data access layer services here, e.g., DbContext, repositories, etc.
+
+        string connectionStringTemplate = configuration.GetConnectionString("MongoDB")!;
+        string connectionString = connectionStringTemplate
+            .Replace("$MONGO_HOST", Environment.GetEnvironmentVariable("MONGODB_HOST")!)
+            .Replace("$MONGO_PORT", Environment.GetEnvironmentVariable("MONGODB_PORT")!);
+
+        services.AddSingleton<IMongoClient>(new MongoClient(connectionString));
+        services.AddScoped<IMongoDatabase>(IConfigurationProvider =>
+        {
+            IMongoClient mongoClient = IConfigurationProvider.GetRequiredService<IMongoClient>();
+            return mongoClient.GetDatabase("eCommerceOrdersDB");
+        });
+
+
         return services;
     }
 }
---
+++ DataAccessLayer/Entities/Orders.cs
@@ -0,0 +1,23 @@
+﻿
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace eCommerce.OrderMicroservice.DataAccessLayer.Entities;
+
+public class Orders
+{
+    [BsonId]
+    [BsonRepresentation(MongoDB.Bson.BsonType.String)]
+    public Guid _id { get; set; }
+
+    [BsonRepresentation(MongoDB.Bson.BsonType.String)]
+    public Guid OrderId { get; set; }
+
+    [BsonRepresentation(MongoDB.Bson.BsonType.String)]
+    public Guid UserId { get; set; }
+
+    [BsonRepresentation(MongoDB.Bson.BsonType.String)]
+    public DateTime OrderDate { get; set; }
+    [BsonRepresentation(MongoDB.Bson.BsonType.Double)]
+    public decimal TotalAmount { get; set; }
+    public List<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+}
